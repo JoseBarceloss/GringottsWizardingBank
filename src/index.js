e592e5649ef080451a3585f33028441040974fad
JoseBarceloss <barcelosps41710@gmail.com>
2024-08-06 13:54:36 -0300

    Criando operation, a lista de actions, fazendo um catch no final para identificar o erro mais facil caso aconteça

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import chalk from 'chalk';
 import inquirer from 'inquirer';
 import fs from 'fs';
+
+operation()
+
+function operation() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+            choices: [
+                'Criar Conta',
+                'Consultar Saldo',
+                'Depositar',
+                'Sacar',
+                'Sair'
+            ]
+    }]).then((answer) => {
+        const action = answer['action']
+
+        console.log(action)
+    })
+    .catch((err) => console.log('Erro na Function operation:',err))
+}
+
