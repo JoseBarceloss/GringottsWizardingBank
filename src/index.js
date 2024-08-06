72f4bcc282a816dae0060300bc04e4ecbbf275bd
JoseBarceloss <barcelosps41710@gmail.com>
2024-08-06 14:30:24 -0300

    Finalizando Criação de conta

--- src/index.js
+++ src/index.js
@@ -26,9 +26,47 @@ function operation() {
     .catch((err) => console.log('Erro na Function operation:',err))
 }
 
-// criar uma conta "vvvv"
+// criar uma conta VVV
 
 function createAccount() {
     console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
     console.log(chalk.green('Defina as opções da sua conta a seguir'))
+
+    buildAccont()
+}
+
+// buildar a conta agora VVVV
+
+function buildAccont() {
+
+    inquirer.prompt([{
+        name: 'accountName',
+        message: 'Digite um nome para a sua conta: '
+    }]).then(answer => {
+        const accountName = answer['accountName']
+
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(
+                chalk.bgRed.black('Esta conta já existe, escolha outro nome!')
+            )
+            buildAccont()
+            return
+        }
+
+        fs.writeFileSync(
+            `accounts/${accountName}.json`, 
+            '{"balance": 0}', 
+            function (err) {console.log('Erro na criação do json accountName: ',err)})
+        
+        console.log(chalk.green('Parabéns, a sua conta foi criada!'))
+        operation()
+
+    }).catch(err => console.log('Erro na function buildAccount:', err))
+
 }
